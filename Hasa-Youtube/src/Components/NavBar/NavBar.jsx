545ab6dac98826b6060670e4a9a762f22b49e37f
masubani <masuabni026@gmail.comgit config --global user.email masuabni026@gmail.com>
2024-06-10 16:54:24 +0530

    Hasa-Youtube start project

---
+++ Hasa-Youtube/src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './NavBar.css'
+
+const NavBar = () => {
+
+  return (
+    <nav className='flex-div'>
+        <div></div>
+
+    </nav>
+  )
+}
+
+export default NavBar
